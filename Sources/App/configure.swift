1bbca9b44ea1ae9ccf1eb8059dadd60fb9fb0640
Matthias Kuhs <matthias.mk@mac.com>
2020-11-28 21:41:42 +0100

    Delete Fluent and Mongo

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,10 @@
-import Fluent
-import FluentMongoDriver
 import Leaf
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
     try routes(app)
